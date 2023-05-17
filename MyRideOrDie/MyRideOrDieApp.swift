647615becfba681833aaaf29dc8ca9e0878e23a5
fbara <fbara1@yahoo.com>
2023-05-17 11:57:39 -0500

    Initial Commit

---
+++ MyRideOrDie/MyRideOrDieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRideOrDieApp.swift
+//  MyRideOrDie
+//
+//  Created by Frank Bara on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MyRideOrDieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
